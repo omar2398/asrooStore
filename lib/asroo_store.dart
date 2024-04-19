f4d864aea3b0ce9538eb331d4821ddd7175e6c94
omar2398 <118669490+omar2398@users.noreply.github.com>
2024-04-19 05:34:23 +0200

    init app

---
+++ lib/asroo_store.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AsrooStoreApp extends StatelessWidget {
+  const AsrooStoreApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('omar asro'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:asro_store/asroo_store.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AsrooStoreApp());
+}
