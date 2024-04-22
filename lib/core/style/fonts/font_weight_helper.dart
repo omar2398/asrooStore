ba4399b0e8e2a2aab60bad51eac822637d3b5a77
omar2398 <118669490+omar2398@users.noreply.github.com>
2024-04-22 15:15:04 +0200

    fontFamily

---
+++ lib/core/style/fonts/font_weight_helper.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class fontWeight{
+  fontWeight._();
+  static final FontWeight fontBold = FontWeight.w700;
+  static final FontWeight fontMeduim = FontWeight.w500;
+  static final FontWeight fontRegular = FontWeight.w400;
+}
